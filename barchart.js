485dfb92b260b2867e3e195c6e3473cad9a64dbe
jjmecca <46037675+jjmecca@users.noreply.github.com>
2025-05-03 19:38:13 -0400

    polished up the third slide and added a sex barchart option

--- barchart.js
+++ barchart.js
@@ -1,6 +1,44 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const data = {
+const info = document.getElementById('info1');
+const title = document.getElementById('barchart_title')
+const button = document.getElementById('bar_button');
+
+let bar = true;
+
+const newTableHTML2 = `
+    <h1><strong>Stops by Sex</strong></h1>
+    <p>This bar chart shows police stops by sex, revealing a significant disparity. Men are stopped far more often than women, by a factor of over 15, highlighting a strong gender imbalance in stop-and-frisk practices.<br><br></p>
+    <hr>
+    <div class="legend">
+        <br>
+        <h3>Legend</h3>
+        <ul>
+        <li><strong>1</strong> - Male</li>
+        <li><strong>2</strong> - Female</li>
+        </ul>
+    </div>
+    `;
+const newTableHTML1 = `
+    <h1><strong>Stops by Race</strong></h1>
+    <p>This bar chart shows police stops by race, with People of Color (POC) highlighted in red. The data reveals a clear disparity—POC are stopped more often than white individuals, with Black individuals facing the highest stop rate by far.<br><br></p>
+    <hr>
+    <div class="legend">
+        <br>
+        <h3>Legend</h3>
+        <ul>
+        <li><strong>1</strong> - Black</li>
+        <li><strong>2</strong> - White Hispanic</li>
+        <li><strong>3</strong> - Black Hispanic</li>
+        <li><strong>4</strong> - White</li>
+        <li><strong>5</strong> - Asian / Pacific Islander</li>
+        <li><strong>6</strong> - Middle Eastern/Southwest Asian</li>
+        <li><strong>7</strong> - American Indian/Alaskan Native</li>
+        </ul>
+    </div>
+    `;
+
+let data = {
     labels: ['1','2','3','4','5','6','7'],
     datasets: [{
         label: 'Stop Counts by Race', 
@@ -13,7 +51,7 @@ const data = {
         borderWidth: 1
     }]
 };
-const config = {
+let config = {
     type: 'bar',
     data: data,
     options: {
@@ -54,3 +92,90 @@ new bootstrap.ScrollSpy(document.body, {
     target: '#navbar',
     offset: 70
 });
+
+function changeBar(){
+    bar=!bar;
+    if(!bar){
+        data = {
+            labels: ['1','2'],
+            datasets: [{
+                label: 'Stop Counts by Sex', 
+                data: [15669,1028],
+                backgroundColor: ['rgba(99, 128, 255, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+                borderColor: ['rgb(99, 128, 255)','rgb(255, 99, 132)'],
+                borderWidth: 1
+            }]
+        };
+        config = {
+            type: 'bar',
+            data: data,
+            options: {
+                animation: { duration: 2500 },
+                responsive: true,
+                scales: { y: { beginAtZero: true } },
+                maintainAspectRatio: false
+            },
+        };
+    }else{
+        data = {
+            labels: ['1','2','3','4','5','6','7'],
+            datasets: [{
+                label: 'Stop Counts by Race', 
+                data: [8542, 3001, 1447, 854, 265, 130, 18],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)','rgba(255, 99, 132, 0.2)',
+                    'rgba(78, 78, 78, 0.2)','rgba(255, 99, 132, 0.2)', 
+                    'rgba(255, 99, 132, 0.2)' , 'rgba(255, 99, 132, 0.2)'],
+                borderColor: ['rgb(255, 99, 132)','rgb(255, 99, 132)','rgb(255, 99, 132)','rgb(112, 94, 98)','rgb(255, 99, 132)','rgb(255, 99, 132)','rgb(255, 99, 132)'],
+                borderWidth: 1
+            }]
+        };
+        config = {
+            type: 'bar',
+            data: data,
+            options: {
+                animation: { duration: 2500 },
+                responsive: true,
+                scales: { y: { beginAtZero: true } },
+                maintainAspectRatio: false
+            },
+        };
+    }
+    chartInstance.destroy();
+    chartInstance = new Chart(ctx, config);
+
+    const newHTML = bar ? newTableHTML1 : newTableHTML2;
+
+    gsap.to(info, {
+        opacity: 0,
+        duration: 0.8,
+        x: 20,
+        onComplete: () => {
+            info.innerHTML = newHTML;
+
+            gsap.fromTo(info,
+                { opacity: 0, x: 20 },
+                { opacity: 1, x: 0, duration: 1 }
+            );
+        }
+    });
+
+    gsap.to(title, {
+        opacity: 0,
+        duration: 0.8,
+        overwrite: true,
+        onComplete: () => {
+          const newTableHTML1 = `Stop Counts by Race Description`;
+          const newTableHTML2 = `Stop Counts by Sex`;
+
+          title.innerHTML = bar ? newTableHTML1 : newTableHTML2;
+
+          gsap.fromTo(title,
+            { opacity: 0 },
+            { opacity: 1, duration: 1, overwrite: true }
+          );
+        }
+    });
+
+    button.innerHTML = bar ? `View by Sex` : `View by Race`;
+}
